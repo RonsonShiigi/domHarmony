14f7fdfda476eea56314e9dbd83739fa72b41329
Ronson Shiigi <ronsonshiigi@Ronsons-MacBook-Pro.local>
2018-10-17 23:42:12 -1000

    pau6

--- js/app.js
+++ js/app.js
@@ -10,11 +10,14 @@ fullname.innerHTML = "Ronald Mcdonald";
 age.innerHTML = 63;
 //5. In the div id data section, create a div with an id of job and give it the following information: Clown and Restauranteur.
     var jobby = document.createElement('div');
-    jobby.id = "job"
-    jobby.innerHTML = "Clown and Restauranteur"
+    jobby.id = "job";
+    jobby.innerHTML = "Clown and Restauranteur";
     data.appendChild(jobby);
 //6. In the div id data section, create a div with an id of hobbies and give it the following information: Long romantic walks on the beach, candle light Big Mac dinners and tormenting the King.
-
+var hobbiesBox = document.createElement('div');
+hobbiesBox.id = "hobbies";
+hobbiesBox.innerHTML= "Long romantic walks on the beach, candle light Big Mac dinners and tormenting the King."
+data.appendChild(hobbiesBox);
 //7. In the div id data section, create a div with an id of location and give it the following information: Honolulu, HI.
 
 //8. In the div id data section, create a div with an id of wants and give it the following information: Looking for a Mrs. McDonald.
