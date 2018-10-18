1cf3b52af5888b600e4c08206982d0d9d2b70c20
Ronson Shiigi <ronsonshiigi@Ronsons-MacBook-Pro.local>
2018-10-17 23:38:26 -1000

    pau5

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
 /*Hey Prepsters! Let's help Ronald McDonald find true love by revising his online dating profile!*/
 
 //1. In the nav element, target the span element with the id of 'matCount' and change the number from 0 to 11.
-
+matCount.innerHTML = 11;
 //2. In the nav element, target the span element with the id of 'msgCount' and change the number from 0 to 23.
-
+msgCount.innerHTML = 23;
 //3. In the div id data section, target the div element with the id of 'fullname' and change Full Name to Ronald McDonald.
-
+fullname.innerHTML = "Ronald Mcdonald";
 //4. In the div id data section, target the div element with the id of 'age' and change Age to 63 Years Old.
-
+age.innerHTML = 63;
 //5. In the div id data section, create a div with an id of job and give it the following information: Clown and Restauranteur.
-
+    var jobby = document.createElement('div');
+    jobby.id = "job"
+    jobby.innerHTML = "Clown and Restauranteur"
+    data.appendChild(jobby);
 //6. In the div id data section, create a div with an id of hobbies and give it the following information: Long romantic walks on the beach, candle light Big Mac dinners and tormenting the King.
 
 //7. In the div id data section, create a div with an id of location and give it the following information: Honolulu, HI.
